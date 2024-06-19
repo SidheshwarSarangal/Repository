ca59f102e3dd134f89135e5a8a70a9f15e62aada
SidheshwarSarangal <sidheshwar.sarangal@gmail.com>
2024-06-19 10:15:38 +0530

    Added

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const register = async (userData) => {
+    const response = await axios.post('/api/auth/register', userData);
+    return response.data;
+};
+
+export const login = async (userData) => {
+    const response = await axios.post('/api/auth/login', userData);
+    return response.data;
+};
